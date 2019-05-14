8be6e8bec79e2f2893a219f043151bb9d198bd1c
João Loureiro <joao.loureiro@mindera.com>
2019-05-14 21:22:16 +0100

    Added some alias for work - docker containers and ssh tunnels for databases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -23,3 +23,11 @@ alias f="fop"
 alias V="lvi"
 alias C="lcd"
 alias F="lop"
+
+#Work
+
+alias docker-mysql="docker run -v /home/lex/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7"
+alias docker-udb="docker run -d -p 8888:8080 -e "_JAVA_OPTIONS=-Dlogging.level.org.springframework=INFO" udb"
+alias db_stage1_tunnel="ssh -L 3307:mysql.cplqfjuc8rfm.eu-west-1.rds.amazonaws.com:3306 bastion-stage"
+alias db_prelive_tunnel="ssh -L 3308:acega-pre-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
+alias db_live_tunnel="ssh -L 3309:cbo-acega-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
