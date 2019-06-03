f604d798f1cedf5d6049185b3b65a7852b6d6bf9
João Loureiro <joao.loureiro@mindera.com>
2019-06-03 21:59:29 +0100

    Updated docker alias for mysql and udb in order to avoid generating multiple containers

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -25,10 +25,13 @@ alias C="lcd"
 alias F="lop"
 
 alias maim-sec="maim -s --format=png /dev/stdout | xclip -selection clipboard -t image/png -i"
+
 #Work
 
-alias docker-mysql="docker run -v /home/lex/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7"
-alias docker-udb="docker run -d -p 8888:8080 -e "_JAVA_OPTIONS=-Dlogging.level.org.springframework=INFO" udb"
+alias docker-mysql-run="docker run -v /home/lex/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7"
+alias docker-mysql="docker start amazing_goodall"
+alias docker-udb-run="docker run -d -p 8888:8080 -e "_JAVA_OPTIONS=-Dlogging.level.org.springframework=INFO" udb"
+alias docker-udb="docker start nostalgic_thompson"
 alias db_stage1_tunnel="ssh -L 3307:mysql.cplqfjuc8rfm.eu-west-1.rds.amazonaws.com:3306 bastion-stage"
 alias db_prelive_tunnel="ssh -L 3308:acega-pre-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
 alias db_live_tunnel="ssh -L 3309:cbo-acega-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
