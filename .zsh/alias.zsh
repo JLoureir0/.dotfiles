345b03fd82bb3bfc9f5dab0877c05752365e2745
João Loureiro <joao.loureiro@mindera.com>
2019-06-11 11:29:29 +0100

    Renamed alias for ssh tunnels and updated comments

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -26,12 +26,15 @@ alias F="lop"
 
 alias maim-sec="maim -s --format=png /dev/stdout | xclip -selection clipboard -t image/png -i"
 
-#Work
-
+#Docker
+#MySQL
 alias docker-mysql-run="docker run -v /home/lex/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7"
 alias docker-mysql="docker start amazing_goodall"
+#UDB
 alias docker-udb-run="docker run -d -p 8888:8080 -e "_JAVA_OPTIONS=-Dlogging.level.org.springframework=INFO" udb"
 alias docker-udb="docker start nostalgic_thompson"
-alias db_stage1_tunnel="ssh -L 3307:mysql.cplqfjuc8rfm.eu-west-1.rds.amazonaws.com:3306 bastion-stage"
-alias db_prelive_tunnel="ssh -L 3308:acega-pre-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
-alias db_live_tunnel="ssh -L 3309:cbo-acega-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
+
+#SSH with tunnels
+alias bastion-stage="ssh -L 3307:mysql.cplqfjuc8rfm.eu-west-1.rds.amazonaws.com:3306 bastion-stage"
+alias bastion-prelive="ssh -L 3308:acega-pre-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
+alias bastion-live="ssh -L 3309:cbo-acega-live.cetwe2fvgnkb.eu-west-1.rds.amazonaws.com:3306 bastion-live"
