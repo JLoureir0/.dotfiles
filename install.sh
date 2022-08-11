3c25dcf412909950e5a579b451bf17dae847ddab
João Loureiro <joao.loureiro@mindera.com>
2022-08-11 13:09:47 +0100

    New install.sh generated from README with the new changes.

--- install.sh
+++ install.sh
@@ -4,14 +4,35 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 . ~/.nix-profile/etc/profile.d/nix.sh
 
 nix-env -iA \
-	nixpkgs.stow
+	nixpkgs.stow \
+	nixpkgs.git \
+	nixpkgs.openfortivpn \
+	nixpkgs.alacritty \
+	nixpkgs.emacs \
+	nixpkgs.vim \
+	nixpkgs.neovim \
+    nixpkgs.jetbrains.idea-ultimate
 
 stow allacritty
-[ `uname -s` = 'Linux' ] && stow awesome
-stow doom
-stow git
-stow nvim
-[ `uname -s` = 'Darwin' ] && stow tmux
-stow vim
-stow X11
 stow zsh
+[ `uname -s` = 'Linux' ] && stow awesome && stow X11
+[ `uname -s` = 'Darwin' ] && stow tmux
+stow git
+stow doom
+stow nvim
+stow vim
+
+if [ `uname -s` = 'Darwin' ];
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+if [ `uname -s` = 'Darwin' ];
+then
+    brew install --cask \
+        raycast \
+        sequel-ace \
+        stats
+fi
+
+curl -s "https://get.sdkman.io" | bash
